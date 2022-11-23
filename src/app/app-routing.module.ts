96a9bc17899abe293f9feca28d8d799dfc6b83af
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-11-23 16:25:20 +0100

    feat: Ex 4.3 || Crypto chips list || added component, model, service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { CategoryListComponentModule } from './components/category-list/category-list.component-module';
+import { CategoryServiceModule } from './services/category.service-module';
+import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
+import { CryptoServiceModule } from './services/crypto.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([{
     path: 'products',
-    component: ProductListComponent
-  }]), ProductListComponentModule, ProductsServiceModule],
+    component: ProductListComponent }, {
+    path: 'categories',
+    component: CategoryListComponent }, {
+    path: 'crypto',
+    component: CryptoListComponent }]),
+    ProductListComponentModule, ProductsServiceModule, CategoryListComponentModule, CategoryServiceModule, CryptoListComponentModule, CryptoServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
